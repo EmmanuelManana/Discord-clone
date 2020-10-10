8bafddc854ac007b5b7878af7f046103132728c5
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-10-11 00:55:50 +0200

    Google Authentication done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import React, { useEffect } from "react";
 import Chat from "./components/Chat.js";
 import Sidebar from "./components/Sidebar.js";
 import "./App.css";
-import { selectUser } from "./features/userSlice.js";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Login from "./components/Login.js";
 import { auth } from "./firebase";
 
+// slices
+import { login, logout, selectUser } from "./features/userSlice.js";
+
 const App = () => {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (authUser){
+        console.table(authUser)
         //user is logged
+
+        // push into redux-store
+        // dispatch an action into user slice and pass the payload,
+        // pass object of any type
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName,
+        }))
       }else{
-        //user is logged oout
+        //user is logged out
+        dispatch(logout())
       }
     });
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="app">
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,37 @@
 import React from "react";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
-import '../styles/Sidebar.css'
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import "../styles/Sidebar.css";
 import SidebarChannel from "./SidebarChannel";
-import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import CallIcon from '@material-ui/icons/Call';
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
 import { Avatar } from "@material-ui/core";
-import MicIcon from '@material-ui/icons/Mic';
-import HeadsetIcon from '@material-ui/icons/Headset';
-import SettingsIcon from '@material-ui/icons/Settings';
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { selectUser } from "../features/userSlice";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  //pull from the redux layer.
+  const user = useSelector(selectUser);
 
   return (
-
     <div className="sidebar">
-      
       <div className="sidebar__top">
         <h3> Emmanuel's Room</h3>
         <ExpandMoreIcon />
       </div>
 
       <div className="sidebar__channels">
-
         <div className="sidebar__channelsHeader">
           <div className="sidebar_header">
             <ExpandMoreIcon />
             <h4>Text Channels</h4>
           </div>
 
-          <AddIcon className="sidebar__addChannel"/>
-
+          <AddIcon className="sidebar__addChannel" />
         </div>
 
         <div className="sidebar__channelsList">
@@ -44,27 +44,29 @@ const Sidebar = () => {
 
       <div className="sidebar__voice">
         <SignalCellularAltIcon
-        className="sidebar__voiceIcon"
-        fontSize="large" 
+          className="sidebar__voiceIcon"
+          fontSize="large"
         />
 
         <div className="sidebar__voiceInfo">
           <h3>Voice Connected</h3>
           <p>Stream</p>
         </div>
-        
+
         <div className="sidebar__voiceIcons">
           <InfoOutlinedIcon />
           <CallIcon />
         </div>
-
       </div>
 
       <div className="sidebar__profile">
-        <Avatar className="sidebar__profileAvatar" src=""/>
+        <Avatar
+          className="sidebar__profileAvatar"
+          src={user ? user.photo : ""}
+        />
         <div className="sidebar__profileInfo">
-          <h3>@elmanana</h3>
-          <p>#this is my ID</p>
+          <h3>{user.displayName}</h3>
+          <p>{user.uid.substring(0, 6)}</p>
         </div>
 
         <div className="sidebar__profileIcons">
@@ -72,13 +74,9 @@ const Sidebar = () => {
           <HeadsetIcon />
           <SettingsIcon />
         </div>
-
       </div>
     </div>
-
-
   );
-
 };
 
 export default Sidebar;
